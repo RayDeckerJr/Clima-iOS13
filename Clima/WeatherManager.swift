0531b032929c439be098a23529d15fa89c7a1b06
Ray Decker Jr <73715881+RayDeckerJr@users.noreply.github.com>
2022-08-08 10:32:15 -0400

    1

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -9,28 +9,33 @@
 import Foundation
 struct WeatherManager {
     let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=e4f1b767d31500f0d103a19986f3ea9e&units=imperial"
-
+    
     func fetchWeather(cityName: String){
         let urlString = "\(weatherUrl)&q=\(cityName)"
-        performRequest(urlString: urlString)
+        self.performRequest(urlString: urlString)
     }
-    
     func performRequest(urlString: String){
         if let url = URL(string: urlString){
             let session = URLSession(configuration: .default)
-            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            let task = session.dataTask(with: url) { data, urlResponse, error in
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+                
+            }
             task.resume()
         }
     }
-    
-    func handle(data:Data?, response: URLResponse?, error: Error?) -> Void{
-        if error != nil{
-            print(error!)
-            return
-        }
-        if let safeData = data {
-            let dataString = String(data: safeData, encoding: .utf8)
-            print(dataString)
+    func parseJSON(weatherData: Data){
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.name)
+            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
     }
 }
